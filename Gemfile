3b1754a9b9b8706f27e8df7cda9733047683f03b
hoang <builehoang1997@gmail.com>
2023-11-09 00:50:59 +0700

    updated image uploader

--- Gemfile
+++ Gemfile
@@ -4,7 +4,8 @@ ruby "3.2.2"
 
 # Bundle edge Rails instead: gem "rails", github: "rails/rails", branch: "main"
 gem "rails", "~> 7.1.1"
-
+gem "image_processing",           "1.12.2"
+gem "active_storage_validations", "0.9.8"
 # The original asset pipeline for Rails [https://github.com/rails/sprockets-rails]
 gem "sprockets-rails"
 
--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -1,17 +1,13 @@
 class Micropost < ActiveRecord::Base
   belongs_to :user
+  has_one_attached :image do |attachable|
+    attachable.variant :display, resize_to_limit: [500, 500]
+  end
   default_scope -> { order(created_at: :desc) }
-  mount_uploader :picture, PictureUploader
   validates :user_id, presence: true
   validates :content, presence: true, length: { maximum: 140 }
-  validate  :picture_size
-
-  private
-
-    # Validates the size of an uploaded picture.
-    def picture_size
-      if picture.size > 5.megabytes
-        errors.add(:picture, "should be less than 5MB")
-      end
-    end
+  validates :image, content_type: { in: %w[image/jpeg image/gif image/png],
+  message: "must be a valid image format" },
+  size:         { less_than: 5.megabytes,
+  message:   "should be less than 5MB" }
 end
