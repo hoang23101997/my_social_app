be0938f917a36c36b187c33028df1f6b074faa4f
hoang <builehoang1997@gmail.com>
2023-11-09 22:12:58 +0700

    updated smtp config to send email

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -40,8 +40,8 @@ Rails.application.configure do
     :address        => 'smtp.gmail.com',
     :port           => '587',
     :authentication => :plain,
-    :user_name      => ENV['gmail_username'],
-    :password       => ENV['gmail_password'],
+    :user_name      => 'phanduyhai72199@gmail.com',
+    :password       => '12345AMV@@',
     :domain         => 'heroku.com',
     :enable_starttls_auto => true
   }
